bce5f71da1308ebda99dfc3ff20008709df8edc9
Aurel Bílý <aurel.bily@gmail.com>
2018-04-21 12:55:34 +0100

    vertical wall with tilt

--- lib/P3D.hx
+++ lib/P3D.hx
@@ -14,28 +14,44 @@ class P3D {
   }
   
   public function render(to:Plot, p:P3DPart, ox:Int, oy:Int, oz:Int):Void {
-    if (p.vert) {
-      var c = Trig.cosAngle[p.angle];
-      var s = Trig.sinAngle[p.angle];
-      var bx:Float = ox + p.x;
-      var by:Float = oy + p.y;
-      var bz:Float = oz + p.z;
-      var cx:Float = ox + p.x;
-      var cy:Float = oy + p.y;
-      var cz:Float = oz + p.z;
-      var vi = 0;
-      for (y in 0...p.h) {
-        cx = bx;
-        cy = by;
-        for (x in 0...p.w) {
-          to.plot(cx.floorZ(), cy.floorZ(), cz.floorZ(), p.data[vi++]);
-          cx += c;
-          cy += s;
-        }
-        cz += 1;
+    var mxx:Float = 0; // x input -> x output
+    var mxy:Float = 0; // y input -> x output
+    var myx:Float = 0; // x input -> y output
+    var myy:Float = 0; // y input -> y output
+    var mzx:Float = 0; // x input -> z output
+    var mzy:Float = 0; // y input -> z output
+    //if (p.vert) {
+      mxx = Trig.cosAngle[p.angle] * Trig.cosTilt[p.tilt];
+      mxy = Trig.cosAngle[p.angle] * Trig.sinTilt[p.tilt];
+      myx = Trig.sinAngle[p.angle] * Trig.cosTilt[p.tilt];
+      myy = Trig.sinAngle[p.angle] * Trig.sinTilt[p.tilt];
+      mzx = Trig.sinTilt[p.tilt];
+      mzy = -Trig.cosTilt[p.tilt];
+    //} else {
+    //  
+    //}
+    var bx:Float = ox + p.x;
+    var by:Float = oy + p.y;
+    var bz:Float = oz + p.z;
+    var vi = 0;
+    var lx:Int = -1;
+    var ly:Int = -1;
+    var cx:Int = -1;
+    var cyy:Int = -1;
+    var cyz:Int = -1;
+    var cye:Int = -1;
+    for (y in 0...p.h) for (x in 0...p.w) {
+      for (off in 0...2) {
+        cx  = (bx + mxx * (x + off * .5) + mxy * (y + off * .5)).floorZ();
+        cyy = (bx + myx * (x + off * .5) + myy * (y + off * .5)).floorZ();
+        cyz = (bz + mzx * (x + off * .5) + mzy * (y + off * .5)).floorZ();
+        cye = cyy - cyz;
+        if (cx == lx && cye == ly) continue;
+        lx = cx;
+        ly = cye;
+        to.plot(cx, cyy, cyz, p.data[vi]);
       }
-    } else {
-      
+      vi++;
     }
     for (s in p.sub) render(to, s, ox + p.x, oy + p.y, oz + p.z);
   }
--- lib/STest.hx
+++ lib/STest.hx
@@ -1,8 +1,11 @@
 package lib;
 
 import sk.thenet.app.JamState;
+import sk.thenet.anim.Phaser;
 import sk.thenet.bmp.*;
 import sk.thenet.plat.Platform;
+
+using sk.thenet.FM;
 
 class STest extends JamState {
   var p3d:P3D;
@@ -19,12 +22,19 @@ class STest extends JamState {
     part = new P3DPart(null);
     part.bitmap = Platform.createBitmap(30, 20, 0xFFAA0000);
     part.z = 20;
+    
+    phasers["tilt"] = new Phaser(72, 8);
+    phasers["angle"] = new Phaser(72, 2);
   }
   
   override public function tick() {
     p3d.render(plot, part, 50, 50, 0);
     plot.render(ab);
-    part.angle++;
-    part.angle %= Trig.densityAngle;
+    //part.angle++;
+    //part.angle %= Trig.densityAngle;
+    //part.tilt++;
+    //part.tilt %= Trig.densityTilt;
+    part.angle = ph("angle");
+    part.tilt = (app.mouse.y >> 4).clampI(0, 8); //ph("tilt");
   }
 }
